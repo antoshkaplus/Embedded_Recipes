6190483957850094fe073931f75d4e36d41c8f72
Anton Logunov <antoshkaplus@gmail.com>
2019-07-27 21:57:55 -0400

    gogo

---
+++ ArduinoKitLessons/Lesson9-Passive buzzer/Passive_buzzer/Passive_buzzer.ino
@@ -0,0 +1,15 @@
+/*
+ * Author: MIUZEI
+ * IDE V1.6.9
+ * Email:service@miuzeipro.com
+ */
+#define buzzer 5
+void setup()
+{
+  // generates a 400Hz tone in output pin 8 with 2000ms of duration
+  tone(buzzer, 10, 2000); 
+}
+void loop() 
+{
+
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+
+
+* LED short is minus
